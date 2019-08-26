3f43b8b42b34779950f1d8712bc4aa9a8d11c089
bradleyDouglas <bdougla2@gmail.com>
2019-08-26 11:41:36 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,94 @@
+module.exports = {
+    siteMetadata: {
+        title: `Gatsby / WordPress Starter`,
+        description: `Site description`,
+        author: `@bradleyDouglas`,
+    },
+    plugins: [
+        {
+            resolve: `gatsby-source-graphql`,
+            options: {
+                // This type will contain remote schema Query type
+                typeName: `WPGraphQL`,
+                // This is field under which it's accessible
+                fieldName: `wpgraphql`,
+                // Url to query from. Change the WORDPRESS_URL to the one you need.
+                // url: `WORDPRESS_URL/graphql`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                useResolverUrlLoader: {
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `bozell-gatsby-wp-starter`,
+                short_name: `bozell-starter`,
+                start_url: `/`,
+                background_color: `#9dc000`,
+                theme_color: `#9dc000`,
+                display: `minimal-ui`,
+                icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: path.resolve(__dirname, "src/assets/svg"),
+                },
+            },
+        },
+        //
+        // Un-comment this for webfonts: https://www.gatsbyjs.org/packages/gatsby-plugin-web-font-loader/
+        // {
+        //   resolve: "gatsby-plugin-web-font-loader",
+        //   options: {
+        //     typekit: {
+        //       id: "lft4tcj",
+        //       api: "//use.typekit.net",
+        //     },
+        //     google: {
+        //       families: ['Droid Sans', 'Droid Serif']
+        //     }
+        //   },
+        // },
+        //
+        // There are two options to keep elements persistent across page changes.
+        // By default, we will use the 'Gatsby Plugin Layout', but you can use
+        // either one depending on your needs.
+        // 1) gatsby-plugin-transition link: https://transitionlink.tylerbarnes.ca/
+        // 2) gatsby-plugin-layout: https://www.gatsbyjs.org/packages/gatsby-plugin-layout/
+        //
+        // Transition Link
+        // {
+        //   resolve: "gatsby-plugin-transition-link",
+        //   options: {
+        //     layout: require.resolve(`./src/components/layout.js`),
+        //   },
+        // },
+        //
+        // Gatsby Plugin Layout
+        {
+            resolve: "gatsby-plugin-layout",
+            options: {
+                component: require.resolve(`./src/components/layout.js`),
+            },
+        },
+        //
+        //
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+}
