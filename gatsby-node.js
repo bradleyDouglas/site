e60c6e3e845b8d893c7bcfb8f6a97b77bc7c66d5
bradleyDouglas <bdougla2@gmail.com>
2019-08-26 16:35:10 -0500

    added the createPages default query

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+const createPages = require(`./create/createPages`)
+
+exports.createPages = async ({ actions, graphql }) => {
+    await createPages({ actions, graphql })
+}
---
+++ src/templates/page.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../components/Seo"
+
+const Page = props => {
+  const {
+    location,
+    data: {
+      wpgraphql: { page },
+    },
+  } = props
+  const { title, content } = page
+  return (
+    <>
+      <Seo title={`${page.title}`} />
+      <h1>{title}</h1>
+    </>
+  )
+}
+
+export default Page
+
+export const pageQuery = graphql`
+  query GET_PAGE($id: ID!) {
+    wpgraphql {
+      page(id: $id) {
+        title
+        content
+        uri
+      }
+    }
+  }
+`
